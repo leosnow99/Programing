2f6578d71a95bb780698aaa0f8f8d68593a7ed1b
leo <leosnow@126.com>
2020-11-29 21:08:37 +0800

    11-29

---
+++ src/cow/advanced/three/SelfCrossing.java
@@ -0,0 +1,13 @@
+package cow.advanced.three;
+
+/**
+ * @author LEOSNOW
+ */
+public class SelfCrossing {
+	public static boolean isSelfCrossing(int[] arr) {
+		if (arr == null || arr.length < 4) {
+			return false;
+		}
+		return true;
+	}
+}
