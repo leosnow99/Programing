76ce059f6a95126c83cdec468eafd517e06109ff
leo <leosnow@126.com>
2020-12-03 21:26:26 +0800

    three new algorithm!

---
+++ src/cow/advanced/three/ConvertStrToInt.java
@@ -0,0 +1,15 @@
+package cow.advanced.three;
+
+/**
+ * 将整数字符串转换成整数值
+ * 整数32位 不能0开头
+ * @author LEOSNOW
+ */
+public class ConvertStrToInt {
+	
+	public static boolean isValid(String str) {
+		return true;
+	
+	}
+	
+}
